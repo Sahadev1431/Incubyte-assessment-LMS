d0fd1b081dcffcf35ff90f56ca4190a8255b921b
Sahadev1431 <naisahadev@gmail.com>
2024-08-11 02:10:28 +0530

    object creation

--- src/test/java/TestLibrary.java
+++ src/test/java/TestLibrary.java
@@ -1,8 +1,15 @@
 package test.java;
 
+import main.java.Library;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestLibrary {
+    Library library;
+    @Before
+    public void setup() {
+        library = new Library();
+    }
     @Test
     public void initialTest() {
 
