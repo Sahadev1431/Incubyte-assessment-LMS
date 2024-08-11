4f36a4a668f679d5f7edbd1dc379fa4fecce9e00
Sahadev1431 <naisahadev@gmail.com>
2024-08-11 17:20:24 +0530

    add: one large test case to ensure system works properly

--- src/test/java/TestLibrary.java
+++ src/test/java/TestLibrary.java
@@ -124,4 +124,72 @@ public class TestLibrary {
         List<Book> booksPresentInLibrary = library.viewAvailableBooks();
         assertFalse(booksPresentInLibrary.contains(testbook));
     }
+
+    @Test
+    public void oneLastLargeTestCaseToEnsure() {
+        // add book
+        testbook = new Book("111-222-351", "The God of Small Things", "Arundhati Roy", Year.of(1997));
+        library.addBook(testbook);
+
+        assertEquals(1, library.getNumberOfBooks());
+
+        // throw exception on duplicate ISBN
+        testbook = new Book("111-345-352", "The God of Small Things", "Arundhati Roy", Year.of(1997));
+        assertThrows(DuplicateISBNException.class, () -> {
+            Book nonUnique = new Book("111-345-352", "The God of Small Things", "Arundhati Roy", Year.of(1997));
+        });
+
+
+        // borrow book
+        testbook = new Book("111-222-353", "The God of Small Things", "Arundhati Roy", Year.of(1997));
+        library.addBook(testbook);
+        library.borrowBook("111-222-353");
+        assertEquals(1, library.getNumberOfBooks());
+
+
+        // throw exception on not available book
+        testbook = new Book("111-222-354", "The God of Small Things", "Arundhati Roy", Year.of(1997));
+        library.addBook(testbook);
+
+        library.borrowBook("111-222-354");
+
+        assertThrows(BookNotFoundException.class, () -> {
+            library.borrowBook("111-222-354");
+        });
+
+
+        // throw an exception on returning book that doesn't belong to library
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.returnBook("111-222-355");
+        });
+
+
+        //  throw an exception on returning book that already exist
+        testbook = new Book("111-222-356", "The God of Small Things 5", "Arundhati Roy", Year.of(1997));
+        library.addBook(testbook);
+        assertThrows(IllegalArgumentException.class, () -> {
+            library.returnBook("111-222-356");
+        });
+
+
+        // return book
+        testbook = new Book("111-222-357", "Manorama Mishra", "Maha Kavitha", Year.of(2001));
+        library.addBook(testbook);
+        library.borrowBook("111-222-357");
+        library.returnBook("111-222-357");
+        assertEquals(3, library.getNumberOfBooks());
+
+
+        // available books
+        testbook = new Book("111-222-358", "Manorama Mishra2", "Maha Kavitha2", Year.of(2001));
+        Book test2 = new Book("111-222-359", "Manorama Mishra3", "Maha Kavitha3", Year.of(2001));
+
+        library.addBook(testbook);
+        library.addBook(test2);
+
+        List<Book> booksPresentInLibrary = library.viewAvailableBooks();
+
+        assertTrue(booksPresentInLibrary.contains(testbook));
+        assertTrue(booksPresentInLibrary.contains(test2));
+    }
 }
